e7cd8156657573aacabba9bfa8585cf72dfa67eb
Beexoul <shivarajpaudel.contact@gmail.com>
2023-04-25 11:18:06 +0545

    channel id update

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ async def msg_delete(sent):
 
 @bot.event
 async def on_message(message):
-    if not message.author.bot and message.channel.id == 1100075511385952369:
+    if not message.author.bot and message.channel.id == @ourChannel_Id_here :
         yt_v1 = ['https://www.youtube.com/watch?v=', 'https://youtu.be/']
         msg = message.content
         msg_split = msg.split(' ')
